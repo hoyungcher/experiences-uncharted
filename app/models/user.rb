1c5d5c90d5a20bf42d24b83480e40f112e212c9e
Yung Cher Ho <ycho527@hotmail.com>
2020-03-16 15:53:06 +0800

    Crash-tested validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :bookings
-  validates :email, presence: true, uniqueness: true, format: { with: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/ }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}\z/ }
   validates :password, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
