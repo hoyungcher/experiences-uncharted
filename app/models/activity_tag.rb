4eb1608fb90e7f469bae70b728295f6d0b698ff0
Amanda Ng <amandaleahng@gmail.com>
2020-03-17 16:00:20 +0800

    fixed logout

--- app/models/activity_tag.rb
+++ app/models/activity_tag.rb
@@ -1,6 +1,8 @@
 class ActivityTag < ApplicationRecord
   belongs_to :activity
   belongs_to :tag
+  has_many :reviews, through: :bookings
+
   validates :activity, presence: true
   validates :tag, presence: true
 end
