d1eff5bf0045e3fd2ac2c8fb27a0937d37a08bcf
Amanda Ng <amandaleahng@gmail.com>
2020-03-17 11:27:37 +0800

    parrots killed

--- db/schema.rb
+++ db/schema.rb
@@ -56,30 +56,6 @@ ActiveRecord::Schema.define(version: 2020_03_17_031850) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "parrots", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_parrots_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_parrots_on_reset_password_token", unique: true
-  end
-
-  create_table "robins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_robins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_robins_on_reset_password_token", unique: true
-  end
-
   create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
