605c48cdbbe38eb8fa8c7dd9655ade57a1390668
Amanda Ng <amandaleahng@gmail.com>
2020-03-17 14:27:09 +0800

    register and login working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'activities#index'
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [ :show, :new, :create ] do
+  resources :users, only: [ :show ] do
     resources :bookings, only: [ :index, :show]
   end
   resources :hosts, only: [ :show ] do
