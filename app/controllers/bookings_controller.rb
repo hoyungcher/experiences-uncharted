63b3aa402d67970d6c0565eac77c170b7a44ce80
Kevan Koh <kevankohjinwen@gmail.com>
2020-03-17 11:42:59 +0700

    added booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,22 +9,21 @@ class BookingsController < ApplicationController
   end
 
   def index
-    @bookings = Booking.all
+    @user = User.find(params[:user_id])
+    @bookings = @user.bookings
   end
 
   def new
     @activity = Activity.find(params[:activity_id])
-    @host = @activity.host
     @user = User.find(params[:user_id])
-    @date = Date.today
-    @booking = Booking.new(activity: @activity, host: @host, user: @user, date: @date)
+    @booking = Booking.new
   end
 
   def create
     @activity = Activity.find(params[:activity_id])
     @host = @activity.host
     @user = User.find(params[:user_id])
-    @date = Date.today
+    @date = [params[:booking]["date(1i)"], params[:booking]["date(2i)"], params[:booking]["date(3i)"]].join("-")
     @booking = Booking.new(activity: @activity, host: @host, user: @user, date: @date)
     if @booking.save
       redirect_to user_bookings_path(@user)
@@ -35,6 +34,6 @@ class BookingsController < ApplicationController
     @user = User.find(params[:user_id])
     @booking = Booking.find(params[:id])
     @booking.destroy
-    redirect_to user_path(@user)
+    redirect_to user_path(@user_id)
   end
 end
