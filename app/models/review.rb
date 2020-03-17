ea1273babf6f86dbb5673d0b8387febb7b2e3b29
Amanda Ng <amandaleahng@gmail.com>
2020-03-17 17:03:10 +0800

    reviews now shown in activity

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,6 @@ class Review < ApplicationRecord
   belongs_to :booking
 
   validates :content, presence: true
+  validates :rating, inclusion: { in: [1, 2, 3, 4, 5],
+    message: "%{value} is not a valid number" }
 end
