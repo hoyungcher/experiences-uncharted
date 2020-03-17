4c87b9e2cf95a286951411c7d13ed58b4416830a
Amanda Ng <amandaleahng@gmail.com>
2020-03-17 16:53:51 +0800

    new/create reviews working

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [ :show ] do
     resources :bookings, only: [ :index, :show] do
-      resources :review, only: [ :new, :create ]
+      resources :reviews, only: [ :new, :create ]
     end
   end
   resources :hosts, only: [ :show ] do
