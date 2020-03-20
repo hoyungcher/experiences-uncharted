4d9a39682719bbd9f5aff21476201d6c3572254f
Amanda Ng <amandaleahng@gmail.com>
2020-03-20 11:50:43 +0800

    navbar fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def role_helper
+    if controller.class == Devise::SessionsController
+     "experience-seeker"
+    elsif controller.class == SessionsController
+      "host"
+    else
+    "stranger"
+    end
+  end
 end
