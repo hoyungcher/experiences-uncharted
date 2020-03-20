c9c4f7307d930f00e6f735aaee81b9eab7909ec8
Yung Cher Ho <ycho527@hotmail.com>
2020-03-20 12:02:29 +0800

    Edited CSS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  SPANISH = { select_city: "Elije una ciudad",
+              select_activity: "#Elije una actividad",
+              orange: "#E0512B",
+              green: "#009551",
+              blue: "#46D0E6",
+              brown: "#D3944A",
+              clear: "#FFFFFF"}
 end
